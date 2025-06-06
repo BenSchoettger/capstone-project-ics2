bb59164cb17d35db0272d6d90ee403a070869aec
Ben Schoettger <ben.schoettger@student.csd509j.net>
2025-06-06 09:20:49 -0700

    made a map

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-let mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
+function loadLevel () {
+    mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 100)
+    scene.setBackgroundImage(assets.image`background1`)
+    tiles.setCurrentTilemap(tilemap`level1`)
+}
+let mySprite: Sprite = null
+loadLevel()
